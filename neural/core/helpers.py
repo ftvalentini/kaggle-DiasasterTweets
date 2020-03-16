27a6a327014c06914c3e40593f7aff28abf1e16e
ftvalentini <ft.valentini@gmail.com>
2020-03-16 00:01:02 -0300

    remove old code

--- neural/core/helpers.py
+++ neural/core/helpers.py
@@ -5,8 +5,6 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import Callback
 from sklearn.metrics import f1_score, recall_score, precision_score
-# from keras.callbacks import K
-# from IPython.display import clear_output
 
 class Metrics(Callback):
     def on_train_begin(self, logs={}):
@@ -17,8 +15,6 @@ class Metrics(Callback):
         X_val = self.validation_data[0]
         y_val = self.validation_data[1]
         y_predict = (np.asarray(self.model.predict(X_val))).round()
-        # y_val = np.argmax(y_val, axis=1)
-        # y_predict = np.argmax(y_predict, axis=1)
         val_f1 = f1_score(y_val, y_predict)
         val_recall = recall_score(y_val, y_predict)
         val_precision = precision_score(y_val, y_predict)
@@ -81,40 +77,40 @@ def create_seqs(X_train, X_val, vocab_size=None, pad_type='pre', seq_maxlen=100)
     vocab_size = len(tokenizer.word_index) + 1
     return tokenizer, X_train_padded, X_val_padded
 
-class LearningPlot(Callback):
-    """
-    Deprecated porque mucho quilombo / remplazado por PlotLossesCallback
-    """
-    def __init__(self, epochs):
-        self.epochs = epochs
-    def on_train_begin(self, logs={}):
-        self.i = 1
-        self.epoch = []
-        self.acc = []
-        self.val_acc = []
-        self.fig, self.ax = plt.subplots(figsize=(8,5))
-        self.lines, = self.ax.plot([],[])
-        self.ax.set_xlim([0,self.epochs+1])
-        self.ax.set_ylim(top=1.0)
-        # self.fig = plt.figure()
-        # self.ax = self.fig.add_subplot(1,1,1)
-        # plt.ion()
-    def on_epoch_end(self, epoch, logs={}):
-        self.epoch.append(self.i)
-        self.acc.append(logs.get('acc'))
-        self.val_acc.append(logs.get('val_acc'))
-        self.i += 1
-        if len(self.epoch) > 1:
-            # f, ax = plt.subplots(1, 1, figsize=(8, 5))
-            # clear_output(wait=True)
-            self.lines.set_xdata(self.epoch)
-            self.lines.set_ydata(self.acc)
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
-            plt.show()
-            # self.ax.plot(self.epoch, self.acc, label="Training Acc.")
-            # self.ax.plot(self.epoch, self.val_acc, label="Validation Acc.")
-            # self.fig.canvas.draw()
-            # self.fig.canvas.flush_events()
-            # plt.draw()
-            # plt.clf()
+# class LearningPlot(Callback):
+#     """
+#     Deprecated porque mucho quilombo / remplazado por PlotLossesCallback
+#     """
+#     def __init__(self, epochs):
+#         self.epochs = epochs
+#     def on_train_begin(self, logs={}):
+#         self.i = 1
+#         self.epoch = []
+#         self.acc = []
+#         self.val_acc = []
+#         self.fig, self.ax = plt.subplots(figsize=(8,5))
+#         self.lines, = self.ax.plot([],[])
+#         self.ax.set_xlim([0,self.epochs+1])
+#         self.ax.set_ylim(top=1.0)
+#         # self.fig = plt.figure()
+#         # self.ax = self.fig.add_subplot(1,1,1)
+#         # plt.ion()
+#     def on_epoch_end(self, epoch, logs={}):
+#         self.epoch.append(self.i)
+#         self.acc.append(logs.get('acc'))
+#         self.val_acc.append(logs.get('val_acc'))
+#         self.i += 1
+#         if len(self.epoch) > 1:
+#             # f, ax = plt.subplots(1, 1, figsize=(8, 5))
+#             # clear_output(wait=True)
+#             self.lines.set_xdata(self.epoch)
+#             self.lines.set_ydata(self.acc)
+#             self.fig.canvas.draw()
+#             self.fig.canvas.flush_events()
+#             plt.show()
+#             # self.ax.plot(self.epoch, self.acc, label="Training Acc.")
+#             # self.ax.plot(self.epoch, self.val_acc, label="Validation Acc.")
+#             # self.fig.canvas.draw()
+#             # self.fig.canvas.flush_events()
+#             # plt.draw()
+#             # plt.clf()
